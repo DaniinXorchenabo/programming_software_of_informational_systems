4608b3f7efa607a62202c1794c641103d080c498
DaniinXorchenabo <rkbcu@mail.ru>
2023-04-07 12:40:14 +0300

    :recycle: refactor.

--- lab_5/selects.sql
+++ lab_5/selects.sql
@@ -33,43 +33,39 @@ where employee.id = ANY (select sub.id
                                group by employee.id) AS sub
                          where sub.min_engine_capacity > 1);
 
-SELECT array_agg (c2.firstname || ' ' || c2.surname), car.id
+SELECT array_agg(c2.firstname || ' ' || c2.surname), car.id
 from car
-right join cheque c on car.id = c.car
-left join client c2 on c2.id = c.client
+         right join cheque c on car.id = c.car
+         left join client c2 on c2.id = c.client
 where c2.firstname LIKE 'М%'
 group by car.id;
 
-SELECT count(cheque.employee),e.id, e.date_of_birth
+SELECT count(cheque.employee), e.id, e.date_of_birth
 from cheque
-right join employee e on e.id = cheque.employee
-where  date_part('year', ( now())) - date_part('year', ( e.date_of_birth)) > 50
+         right join employee e on e.id = cheque.employee
+where date_part('year', (now())) - date_part('year', (e.date_of_birth)) > 50
 group by e.id;
 
 
 SELECT
 --     c.year_of_release,
-    *
+*
 from client
 -- right join cheque c3 on client.id = c3.client
 -- left join car c on c.id = c3.car
-where client.id = any(
-    select c2.id
-    from car
-    right outer join cheque c on car.id = c.car
-    left outer join client c2 on c2.id = c.client
-    where car.year_of_release < '2005-01-01'::date
-    );
+where client.id = any (select c2.id
+                       from car
+                                right outer join cheque c on car.id = c.car
+                                left outer join client c2 on c2.id = c.client
+                       where car.year_of_release < '2005-01-01'::date);
 
 
-select  c.color_name, *
+select c.color_name, *
 from car
-left outer join color c on c.id = car.color
-where char_length(c.color_name) = (
-select max(char_length(col_.color_name))
-from color as col_
-right join public.car c2 on col_.id = c2.color
-);
+         left outer join color c on c.id = car.color
+where char_length(c.color_name) = (select max(char_length(col_.color_name))
+                                   from color as col_
+                                            right join public.car c2 on col_.id = c2.color);
 
 -------------------
 SELECT max(max_speed)
